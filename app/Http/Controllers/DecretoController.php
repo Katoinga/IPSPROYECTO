0ff47c3bba5539fdedfad97d178c72e1a0c2accd
Sergio Soncco <axulsc@gmail.com>
2020-06-28 18:21:21 -0500

    Requisito Bajo 1 Implementado

---
+++ app/Http/Controllers/DecretoController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class DecretoController extends Controller{
+    public function view(){
+      return view('decretoSupremo');
+    }
+}
+?>
