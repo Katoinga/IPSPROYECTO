cd16943a6c54b74dbc7a71702223d58927219bd1
Katoinga <evaldiviaci@unsa.edu.pe>
2020-07-08 12:53:50 -0500

    v2.4

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('chart', 'ChartController@index');
 Route::get('decretoSupremo','DecretoController@view');
 Route::resource('simple','Filtro2Controller');
 /*Route::get('universidades','UniversidadesController@index');*/
