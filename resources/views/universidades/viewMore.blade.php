4333e83002a98c4fad37fbe984d6c2446b5dc933
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2020-07-14 00:39:31 -0500

    Busqueda Css

--- resources/views/universidades/viewMore.blade.php
+++ resources/views/universidades/viewMore.blade.php
@@ -1,6 +1,10 @@
 <html>
 <head>
 	<title>UNIVERSIDADES ACREDITADAS</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </head>
 <body>
 	<div id="header" class="container">
@@ -17,11 +21,79 @@
 			</div>
 		</div>
 	</div>
+	<div class="container">
+		<nav class="navbar navbar-light bg-light">
+		  <a class="navbar-brand">Filtros de Busqueda</a>
+			<form class="form-inline">
+			  <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Región</label>
+				<select class="custom-select my-1 mr-sm-2" name="queryRegion" id="inputRegion">
+			    <option value="">Ninguno...</option>
+					<option>Amazonas</option>
+					<option>Ancash</option>
+					<option>Apurimac</option>
+					<option>Arequipa</option>
+					<option>Ayacucho</option>
+					<option>Cajamarca</option>
+					<option>Callao</option>
+					<option>Cusco</option>
+					<option>Huancavelica</option>
+					<option>Huanuco</option>
+					<option>Ica</option>
+					<option>Junín</option>
+					<option>La Libertad</option>
+					<option>Lambayeque</option>
+					<option>Lima</option>
+					<option>Loreto</option>
+					<option>Madre de Dios</option>
+					<option>Moquegua</option>
+					<option>Pasco</option>
+					<option>Piura</option>
+					<option>Puno</option>
+					<option>San Martín</option>
+					<option>Tacna</option>
+					<option>Tumbes</option>
+					<option>Ucayali</option>
+			  </select>
+				<button class="btn btn-outline-dark" type="submit">Aplicar</button>
+			</form>
+		</nav>
 
+
+		<table class="table table-striped">
+		  <thead>
+		    <tr class="thead-dark">
+		      <th scope="col">Universidad</th>
+		      <th scope="col">Gestion</th>
+		      <th scope="col">Estado de Licenciamiento</th>
+		      <th scope="col">Periodo de Licenciamiento</th>
+					<th scope="col">Departamento</th>
+					<th scope="col">Provincia</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+				<?php
+        if($universidades ?? ''):
+          foreach($universidades ?? '' as $universidad):?>
+		      <tr>
+					       <td><?php echo $universidad->nombre;?></td>
+					       <td><?php echo $universidad->tipo_gestion ?></td>
+					       <td><?php echo $universidad->estado_licenciamiento ?></td>
+                 <td><?php echo $universidad->periodo_licenciamiento ?></td>
+					       <td><?php echo $universidad->departamento ?></td>
+					       <td><?php echo $universidad->provincia ?></td>
+		      </tr>
+          <?php
+          endforeach;
+				endif;
+        ?>
+		  </tbody>
+		</table>
+	</div>
+	<!--Tabla Antigua de Sergito
 	<nav class="navbar">
 	  <a class="navbar-brand">Filtros de Busqueda</a>
-	  <form class="form">
-			<label >Region</label>
+	  <form class="form-inline">
+			<label class="my-1 mr-2" for="inlineFormCustomSelectPref">Region</label>
       <select name="queryRegion" id="inputRegion">
 				<option value="">Ninguno</option>
         <option>Amazonas</option>
@@ -80,6 +152,7 @@
 				endif;
         ?>
 		</table>
+		-->
 	<div id="copyright" class="container">
 		<h3>Contactanos</h3>
 		<ul>
