11018d1be048c09f2e31c2923b620923f9b2de51
Sergio Soncco <axulsc@gmail.com>
2020-07-19 22:11:50 -0500

    RF7 y arreglos a layout v2

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,10 +1,9 @@
 <html>
 <head>
 	<title>UNIVERSIDADES ACREDITADAS</title>
-<<<<<<< HEAD
+
 	<link rel="stylesheet" href="{{ asset('css/estilo.css') }}" />
-=======
->>>>>>> 56e532e42882d13932827c58f019b53065b98e4f
+
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -19,11 +18,8 @@
 				<a href="universidades" class="btn btn-secondary" role="button">Universidades</a>
         <a href="reports" class="btn btn-secondary" role="button">Reportes</a>
         <a href="decretoSupremo" class="btn btn-secondary" role="button">Decreto</a>
-<<<<<<< HEAD
         <br>
-=======
 <br>
->>>>>>> 56e532e42882d13932827c58f019b53065b98e4f
     	</ul>
 		</div>
 	</div>
@@ -32,19 +28,11 @@
 	<div id="copyright" class="container">
 		<h3>Contactanos</h3>
 		<ul>
-<<<<<<< HEAD
 			<li>cgonzalesmo@unsa.edu.pe
 			<li>ssonccoc@unsa.edu.pe
 			<li>evaldiviaci@unsa.edu.pe
 			<li>mcarpioco@unsa.edu.pe
 			<li>fbarrientos@unsa.edu.pe
-=======
-			<li><p>cgonzalesmo@unsa.edu.pe</p>
-			<li><p>ssonccoc@unsa.edu.pe</p>
-			<li><p>evaldiviaci@unsa.edu.pe</p>
-			<li><p>mcarpioco@unsa.edu.pe</p>
-			<li><p>fbarrientos@unsa.edu.pe</p>
->>>>>>> 56e532e42882d13932827c58f019b53065b98e4f
 		</ul>
 	</div>
 
